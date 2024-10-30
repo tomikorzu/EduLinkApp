83edd4cb1f321a43da34603e4b3390ef8a32e0ab
tomikorzu <tomykorzu@icloud.com>
2024-10-30 20:26:32 -0300

    feat(routes/users.js): add post route to add users

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,25 @@ import { validateAll } from "../middlewares/validateUsers.js";
 
 const router = express.Router();
 
-router.post("/", validateAll, (req, res) => {});
+router.post("/", validateAll, (req, res) => {
+  const { username, email, password, fullname } = req.body;
+  db.run(
+    `INSERT INTO users (username, email, password, fullname) VALUES (?, ?, ?, ?)`,
+    [username, email, password, fullname],
+    (err) => {
+      if (err) {
+        res.status(500).json({ message: "There was a problem in the server" });
+      }
+      res.status(201).json({
+        message: "User added successfully",
+        id: this.lastId,
+        username,
+        email,
+        password,
+        fullname,
+      });
+    }
+  );
+});
 
 export default router;
