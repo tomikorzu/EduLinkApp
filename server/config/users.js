cf9a4b2df05e86672879d9f1ff6c5e6ed5e11bae
tomikorzu <tomykorzu@icloud.com>
2024-10-30 20:59:43 -0300

    feat(config.js / routes.js): connect the signup to the database

--- server/config/users.js
+++ server/config/users.js
@@ -2,6 +2,14 @@ import sqlite from "sqlite3";
 
 const db = new sqlite.Database("./server/databases/users.db");
 
-db.serialize(() => {});
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE, 
+            password TEXT NOT NULL,
+            fullname TEXT NOT NULL
+        )`);
+});
 
 export default db;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,38 @@
 import express from "express";
 import db from "../config/users.js";
+import bcrypt from "bcrypt";
 
 import { validateAll } from "../middlewares/validateUsers.js";
 
 const router = express.Router();
 
-router.post("/", validateAll, (req, res) => {
+router.post("/register", validateAll, async (req, res) => {
   const { username, email, password, fullname } = req.body;
-  db.run(
-    `INSERT INTO users (username, email, password, fullname) VALUES (?, ?, ?, ?)`,
-    [username, email, password, fullname],
-    (err) => {
-      if (err) {
-        res.status(500).json({ message: "There was a problem in the server" });
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    db.run(
+      `INSERT INTO users (username, email, password, fullname) VALUES (?, ?, ?, ?)`,
+      [username, email, hashedPassword, fullname],
+      function (err) {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "There was a problem in the server" });
+        }
+        res.status(201).json({
+          message: "User added successfully",
+          id: this.lastID,
+          username,
+          email,
+          hashedPassword,
+          fullname,
+        });
       }
-      res.status(201).json({
-        message: "User added successfully",
-        id: this.lastId,
-        username,
-        email,
-        password,
-        fullname,
-      });
-    }
-  );
+    );
+  } catch (err) {
+    res.status(500).json({ message: "Error hashing password" });
+  }
 });
 
 export default router;
