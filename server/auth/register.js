12f78a15ed712285f9a4d64138ca3fe6d96f5740
tomikorzu <tomykorzu@icloud.com>
2024-11-01 21:04:05 -0300

    fix(register): make the legible of the code better

--- server/auth/register.js
+++ server/auth/register.js
@@ -3,59 +3,43 @@ import bcrypt from "bcrypt";
 
 import roles from "../constants/roles.js";
 
-export const register = async (req, res) => {
+export const register = (req, res) => {
   const { username, email, password, fullname } = req.body;
 
-  try {
-    const userExists = await new Promise((resolve, reject) => {
-      db.get(
-        `SELECT username, email FROM users WHERE username = ? OR email = ?`,
-        [username, email],
-        (err, row) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(row);
-          }
-        }
-      );
-    });
+  db.get(
+    `SELECT username, email FROM users WHERE username = ? OR email = ?`,
+    [username, email],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ message: "There was a server problem" });
+      }
 
-    if (userExists) {
-      if (userExists.username === username) {
-        return res.status(409).json({ message: "Username already exists" });
-      } else if (userExists.email === email) {
-        return res.status(409).json({ message: "Email already exists" });
+      if (result) {
+        if (userExists.username === username) {
+          return res.status(409).json({ message: "Username already exists" });
+        } else if (userExists.email === email) {
+          return res.status(409).json({ message: "Email already exists" });
+        }
       }
     }
+  );
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = bcrypt.hash(password, 10);
 
-    await new Promise((resolve, reject) => {
-      db.run(
-        `INSERT INTO users (username, email, password, fullname, role) VALUES (?, ?, ?, ?, ?)`,
-        [username, email, hashedPassword, fullname, roles.student],
-        function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(this.lastID);
-          }
-        }
-      );
-    });
+  db.run(
+    `INSERT INTO users (username, email, password, fullname, role) VALUES (?, ?, ?, ?, ?)`,
+    [username, email, hashedPassword, fullname, roles.student],
+    (err) => {
+      if (err) {
+        res.status(500).json({ message: "There was a server error" });
+      }
 
-    res.status(201).json({
-      message: "User added successfully",
-      username,
-      email,
-      fullname,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "There was a problem in the server",
-      error: err.message,
-    });
-  }
+      res.status(201).json({
+        message: "User added successfully",
+        username,
+        email,
+        fullname,
+      });
+    }
+  );
 };
