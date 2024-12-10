c7290d042597983fe2b9b75ba96b782563b016c2
tomikorzu <tomykorzu@icloud.com>
2024-12-10 17:17:15 -0300

    feat(fetch/auth): add this function to reuse it when the client need to fetch data

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,20 @@
 "use client";
 
-import { useState } from "react";
 import { fetchAuth } from "@/utils/fetch/auth";
+import { useState } from "react";
 
-export default function RegisterPage() {
-  const [fullname, setFullname] = useState("");
+export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   async function handleSumbit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const res = await fetchAuth({ fullname, email, password }, "register");
+    const res = await fetchAuth({ email, password }, "login");
     console.log(res);
   }
-
   return (
     <main className="flex items-center justify-center min-h-screen bg-[#555]">
       <form onSubmit={handleSumbit} className="bg-[#333] flex flex-col gap-2 ">
-        <input
-          type="text"
-          value={fullname}
-          onChange={(e) => setFullname(e.target.value)}
-        />
         <input
           type="email"
           value={email}
@@ -33,7 +26,7 @@ export default function RegisterPage() {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit" className="bg-[#2ee01d] text-[#f1f1f1]">
-          Register
+          Login
         </button>
       </form>
     </main>
