27f0492cfdaf58a33ff8629f5c2642d7047e4e84
tomikorzu <tomykorzu@icloud.com>
2024-12-11 06:11:32 -0300

    feat(CreateCourse): add little changes to make this more clear

--- src/shared/components/SideBar/components/CreateCourse/CreateCourseBtn.tsx
+++ src/shared/components/SideBar/components/CreateCourse/CreateCourseBtn.tsx
@@ -1,16 +1,22 @@
 import CreateCourseModal from "./CreateCourseModal";
 
+import { useState } from "react";
+
 export default function CreateCourseBtn() {
+  const [showModal, setShowModal] = useState(false);
   function createTask() {
-    <CreateCourseModal />;
+    setShowModal(true);
   }
   return (
-    <button
-      onClick={createTask}
-      type="button"
-      className="flex items-center gap-1 p-2 bg-[#10da32] rounded-md mx-auto my-4 hover:brightness-110 transition duration-500"
-    >
-      <i className="fa-solid fa-plus"></i> <span>Create Class</span>
-    </button>
+    <>
+      <button
+        onClick={createTask}
+        type="button"
+        className="flex items-center gap-1 p-2 bg-[#10da32] rounded-md mx-auto my-4 hover:brightness-110 transition duration-500"
+      >
+        <i className="fa-solid fa-plus"></i> <span>Create Course</span>
+      </button>
+      {showModal && <CreateCourseModal setShowModal={setShowModal} />}
+    </>
   );
 }
--- src/shared/components/SideBar/components/CreateCourse/CreateCourseModal.tsx
+++ src/shared/components/SideBar/components/CreateCourse/CreateCourseModal.tsx
@@ -2,30 +2,40 @@ import { useContext, useState } from "react";
 import { AuthContext } from "@/shared/providers/auth";
 import { fetchData } from "@/utils/fetch/data";
 
-export default function CreateCourseModal() {
+export default function CreateCourseModal({
+  setShowModal,
+}: {
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const [courseName, setCourseName] = useState("");
   const [courseCode, setCourseCode] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+  const [courseVisibility, setCourseVisibility] = useState("public");
 
   const { token } = useContext(AuthContext)!;
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const res = await fetchData(
-      "courses",
+      "courses/create",
       "POST",
-      { courseName, courseCode },
+      { courseName, courseCode, courseDescription, courseVisibility },
       {
         Authorization: `Bearer ${token}`,
       }
     );
     if (res.status === 201) {
       console.log(res.data);
+      setShowModal(false);
     } else {
       console.error(res.data.errors);
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col fixed top-[50%] left-[50%] translate-[-50%, -50%] bg-[#333] z-10 text-[#111]"
+    >
       <input
         type="text"
         value={courseName}
@@ -38,6 +48,18 @@ export default function CreateCourseModal() {
         onChange={(e) => setCourseCode(e.target.value)}
         placeholder="Course Code"
       />
+      <textarea
+        value={courseDescription}
+        onChange={(e) => setCourseDescription(e.target.value)}
+        placeholder="Course Description"
+      />
+      <select
+        value={courseVisibility}
+        onChange={(e) => setCourseVisibility(e.target.value)}
+      >
+        <option value="public">Public</option>
+        <option value="private">Private</option>
+      </select>
       <button type="submit" className="bg-[#2ee01d] text-[#f1f1f1]">
         Create Course
       </button>
