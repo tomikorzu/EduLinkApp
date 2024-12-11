46f429713da3ec769101ef7f889df5c0db91e918
tomikorzu <tomykorzu@icloud.com>
2024-12-11 03:34:29 -0300

    feat(sidebarSections): add all sections of sidebar

---
+++ src/shared/components/SideBar/sidebarSections.ts
@@ -0,0 +1,17 @@
+export const sidebarSections = [
+  {
+    icon: "fa fa-home",
+    title: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    icon: "fa fa-user",
+    title: "Profile",
+    path: "/profile",
+  },
+  {
+    icon: "fa fa-cog",
+    title: "Settings",
+    path: "/settings",
+  },
+];
