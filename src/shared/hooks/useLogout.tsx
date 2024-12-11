a02c19108783575d00922fdfe6f6c084a29ac9b5
tomikorzu <tomykorzu@icloud.com>
2024-12-11 03:32:45 -0300

    feat(useLogout): add this hook to use it when the client press logout button

---
+++ src/shared/hooks/useLogout.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/auth";
+import { useRouter } from "next/navigation";
+import { AlertContext } from "../providers/alert";
+
+export function useLogout() {
+  const { setIsAuthenticate } = useContext(AuthContext);
+  const { showAlert } = useContext(AlertContext);
+
+  const router = useRouter();
+  const logout = () => {
+    sessionStorage.clear();
+    localStorage.clear();
+    setIsAuthenticate(false);
+    showAlert("Logout successfully", false);
+    router.push("/login");
+  };
+  return logout;
+}
