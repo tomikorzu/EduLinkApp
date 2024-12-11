b123ea58841a7f86bb9136593389ff5d54e96186
tomikorzu <tomykorzu@icloud.com>
2024-12-10 21:35:55 -0300

    feat(login): finish this endpoint with the validations

--- src/pages/api/auth/login/index.ts
+++ src/pages/api/auth/login/index.ts
@@ -1,11 +1,21 @@
+import { runMiddleware } from "@/utils/middlewares/runMiddleware";
 import { NextApiRequest, NextApiResponse } from "next";
+import { loginMiddleware, validateErrors } from "./loginMiddleware";
 
 export default async function LoginHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    return res.status(200).json({ message: "Login is here" });
+    await runMiddleware(req, res, [...loginMiddleware]);
+
+    const errorsMessages = validateErrors(req);
+
+    if (errorsMessages) {
+      return res.status(400).json({ errors: errorsMessages });
+    }
+
+    return res.status(200).json({ message: "Logged successfully!" });
   } else {
     return res.status(405).json({ error: "Method not allowed" });
   }
